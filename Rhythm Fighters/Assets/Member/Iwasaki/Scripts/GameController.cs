462b01f1d45cb4e3ba02dc4ebb7444aa126e7a0e
yata <yuuki0523yata@gmail.com>
2019-10-25 16:44:45 +0900

    それぞれのファイルの作成

--- Rhythm Fighters/Assets/Member/Iwasaki/Scripts/GameController.cs
+++ Rhythm Fighters/Assets/Member/Iwasaki/Scripts/GameController.cs
@@ -6,15 +6,20 @@ public class GameController : MonoBehaviour
 {
     [SerializeField]
     private GameObject checkBox;
+    [SerializeField]
+    private AudioSource audioSource;
 
     public static int notesSpeed;
     public static int BPM;
     public int BPMNum;
 
+    private float tes;
     void Start()
     {
         BPM = BPMNum;
         notesSpeed = BPMNum / 20;
+        
+        //tes = (60 * 4) / BPM * 
     }
 
     void Update()
