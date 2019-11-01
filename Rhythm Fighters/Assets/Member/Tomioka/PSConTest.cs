ddd9de0eb4f7fb05872f5d0dffd1d06263e16d67
Tomioka <17c1020019tt@edu.tech.ac.jp>
2019-11-01 18:18:22 +0900

    PSのコントローラーを押せるようにする

---
+++ Rhythm Fighters/Assets/Member/Tomioka/PSConTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSConTest : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Left") > 0.5f)
+        {
+            Debug.Log("右");
+        }
+        if (Input.GetAxis("Left") < -0.5f)
+        {
+            Debug.Log("左");
+        }
+        if (Input.GetButtonDown("Maru"))
+        {
+            Debug.Log("〇");
+        }
+        if (Input.GetButtonDown("Batu"))
+        {
+            Debug.Log("×");
+        }
+        if (Input.GetButtonDown("Sankaku"))
+        {
+            Debug.Log("△");
+        }
+        if (Input.GetButtonDown("Sikaku"))
+        {
+            Debug.Log("□");
+        }
+    }
+}
