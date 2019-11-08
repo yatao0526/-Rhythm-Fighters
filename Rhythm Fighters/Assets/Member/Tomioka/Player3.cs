3bd1b4ec60c6bacafc80f18a742ff476adf8b4db
Tomioka <17c1020019tt@edu.tech.ac.jp>
2019-11-08 18:08:53 +0900

    キャラ移動1個追加

---
+++ Rhythm Fighters/Assets/Member/Tomioka/Player3.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3 : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector3 moveX;
+
+    // 移動時間
+    [SerializeField]
+    private float stepTime;
+
+    //移動後の場所
+    Vector3 moveAfter;
+
+    Vector3 moveBeforePos;
+
+    void Start()
+    {
+        moveAfter = this.transform.position;
+    }
+
+    void Update()
+    {
+
+        if (this.transform.position == moveAfter)
+        {
+            SetTargetPosition();
+        }
+        Move();
+    }
+
+    void SetTargetPosition()
+    {
+
+        moveBeforePos = moveAfter;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveAfter = transform.position + moveX;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveAfter = transform.position - moveX;
+        }
+
+    }
+
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveAfter, stepTime * 10 * Time.deltaTime);
+    }
+}
+
