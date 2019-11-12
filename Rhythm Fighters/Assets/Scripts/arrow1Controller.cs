aa1146ee06b9345c3ac048205a61826b4d25f1a8
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2019-11-12 23:45:27 +0900

    キャラ選択画面関係
    20191112　作業報告
    完成機能
    1.pvp　1p赤　2p青
    　選択可能のキャラーは6つ（仮）
    　同じキャラーは選択不可能
    2.選択されたキャラーは灰色になる
    //選択された色々は灰色になると誰か選択したのが理解しつらいので　各ＰＬＡＹＥＲの色々にしました　確定したら色が濃くなるようにしました。
    未完成//-----------------
    3.1ｐと2ｐがキャラーを確認すると、ステージの選択画面に転移、ステージを確認すると、次のシーンに転移（戦闘シーン）//ステージ選択画面の仕様を教えてください。
    未完成//-------------

--- Rhythm Fighters/Assets/Scripts/arrow1Controller.cs
+++ Rhythm Fighters/Assets/Scripts/arrow1Controller.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class arrow1Controller : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class arrow1Controller : MonoBehaviour
     public GameObject[] characters = new GameObject[6];
     public GameObject arrow1;
     public GameObject arrow2;
+    public bool player1PCharacter = false;
+    public bool player2PCharacter = false;
+
     //選択されたキャラのナンバー数を設定
 
     // Start is called before the first frame update
@@ -17,11 +22,9 @@ public class arrow1Controller : MonoBehaviour
         
         
         arrow1 = GameObject.Find("arrow1");
-
-
-        
         arrow2 = GameObject.Find("arrow2");
-
+        arrow1.transform.position = new Vector3(characters[charNum_1].transform.position.x, 320, 0);
+        arrow2.transform.position = new Vector3(characters[charNum_2].transform.position.x, 760, 0);
     }
 
     // Update is called once per frame
@@ -31,75 +34,124 @@ public class arrow1Controller : MonoBehaviour
 
         Player2Move();
         Player1Move();
+        if (player1PCharacter && player2PCharacter) {
+            Debug.Log("1P" + characters[charNum_1]+ "2P" + characters[charNum_2]);
 
+            //次のシーンへ
+            SceneManager.LoadScene("Game");
+        }
+ 
     }
     void Player1Move() {
         //--------Player1 Y320
-
+        if (!player1PCharacter) { 
         if (Input.GetKeyDown(KeyCode.A))
         {
             //左へ移動
+            characters[charNum_1].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
             charNum_1--;
-            if (charNum_2 == charNum_1)
-            {
-                charNum_1--;
-            }
+           
             if (charNum_1 < 0) {
                 charNum_1 = 5;      
             }
-           
+            if (charNum_2 == charNum_1)
+            {
+                charNum_1--;
+                if (charNum_1 < 0)
+                {
+                    charNum_1 = 5;
+                }
+            }
+          
+
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-           
+            characters[charNum_1].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
             charNum_1++;
-            if (charNum_1 == charNum_2)
-            {
-                charNum_1++;
-            }
+          
             if (charNum_1 > 5)
             {
                 charNum_1 = 0;
             }
+            if (charNum_1 == charNum_2)
+            {
+                charNum_1++;
+                if (charNum_1 > 5)
+                {
+                    charNum_1 = 0;
+                }
+            }
            
+
         }
         arrow1.transform.position = new Vector3(characters[charNum_1].transform.position.x, 320, 0);
-      
+
+            if (Input.GetKeyDown(KeyCode.E) && !player1PCharacter)
+            {
+                player1PCharacter = true;
+               
+            }
+            if (player1PCharacter) { characters[charNum_1].GetComponent<Image>().color = new Color(255.0f / 255.0f, 8 / 255.0f, 0 / 255.0f, 255 / 255.0f); }
+            else {
+                characters[charNum_1].GetComponent<Image>().color = new Color(255.0f / 255.0f, 8 / 255.0f, 0 / 255.0f, 130 / 255.0f);
+            }
+        }
     }
-    void Player2Move() {
-       
-        //-----------------Player2Y760
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+    void Player2Move()
+    {
+        if (!player2PCharacter)
         {
-
-            charNum_2--;
-            if (charNum_2 == charNum_1)
+            //-----------------Player2Y760
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
+                characters[charNum_2].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
                 charNum_2--;
-            }
-            if (charNum_2 < 0)
-            {
-                charNum_2 = 5;
-            }
-           
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            //左へ移動
-            charNum_2++;
 
-            if (charNum_2 == charNum_1)
+                if (charNum_2 < 0)
+                {
+                    charNum_2 = 5;
+                }
+                if (charNum_2 == charNum_1)
+                {
+                    charNum_2--;
+                    if (charNum_2 < 0)
+                    {
+                        charNum_2 = 5;
+                    }
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
+                characters[charNum_2].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
+                //左へ移動
                 charNum_2++;
+                if (charNum_2 > 5)
+                {
+                    charNum_2 = 0;
+                }
+                if (charNum_2 == charNum_1)
+                {
+                    charNum_2++;
+                    if (charNum_2 > 5)
+                    {
+                        charNum_2 = 0;
+                    }
+                }
+
+
             }
-            if (charNum_2 > 5)
+
+            arrow2.transform.position = new Vector3(characters[charNum_2].transform.position.x, 760, 0);
+
+
+            if (Input.GetKeyDown(KeyCode.Space)&& !player2PCharacter)
             {
-                charNum_2 = 0;
+                player2PCharacter = true;
+             
             }
-
+            if (player2PCharacter) { characters[charNum_2].GetComponent<Image>().color = new Color(0 / 255.0f, 62 / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f); }
+            else { characters[charNum_2].GetComponent<Image>().color = new Color(0 / 255.0f, 62 / 255.0f, 255.0f / 255.0f, 130.0f / 255.0f); }
         }
-        
-        arrow2.transform.position = new Vector3(characters[charNum_2].transform.position.x, 760, 0);
-
     }
 }
