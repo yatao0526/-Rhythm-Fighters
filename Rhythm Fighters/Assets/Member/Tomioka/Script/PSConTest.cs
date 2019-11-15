a8f91954fdb7fb5bf2719e9ef38e4a484c5d30b2
Tomioka <17c1020019tt@edu.tech.ac.jp>
2019-11-15 14:34:42 +0900

    コントローラーの一部変更

--- Rhythm Fighters/Assets/Member/Tomioka/Script/PSConTest.cs
+++ Rhythm Fighters/Assets/Member/Tomioka/Script/PSConTest.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class PSConTest : MonoBehaviour
 {
+    private bool neutralLRPosition = true;
+    private bool neutralDownPosition = true;
 
-    void Start()
-    {
-
-    }
 
     void Update()
     {
@@ -17,18 +15,39 @@ public class PSConTest : MonoBehaviour
 
     private void PSConDebug()
     {
-        if (Input.GetAxis("LeftRight") > 0.5f)
+        //コントローラーの左右
+        #region
+        if (Input.GetAxis("LeftRight") > 0.5f && neutralLRPosition == true)
         {
             Debug.Log("右");
+            neutralLRPosition = false;
         }
-        if (Input.GetAxis("LeftRight") < -0.5f)
+        if (Input.GetAxis("LeftRight") < -0.5 && neutralLRPosition == true)
         {
             Debug.Log("左");
+            neutralLRPosition = false;
         }
-        if (Input.GetAxis("Down") < -0.5f)
+        if (Input.GetAxis("LeftRight") == 0.0f)
+        {
+            neutralLRPosition = true;
+        }
+        #endregion
+
+        //コントローラーの下
+        #region
+        if (Input.GetAxis("Down") < -0.5f && neutralDownPosition == true)
         {
             Debug.Log("下");
+            neutralDownPosition = false;
         }
+        if (Input.GetAxis("Down") == 0.0f)
+        {
+            neutralDownPosition = true;
+        }
+        #endregion
+
+        //〇×△□のボタン
+        #region
         if (Input.GetButtonDown("Maru"))
         {
             Debug.Log("〇");
@@ -45,6 +64,7 @@ public class PSConTest : MonoBehaviour
         {
             Debug.Log("□");
         }
+        #endregion
 
     }
 }
