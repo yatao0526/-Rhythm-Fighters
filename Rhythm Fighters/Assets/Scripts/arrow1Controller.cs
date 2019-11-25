ef5f17fb94ccd1f18a59326670f6e574cf3df59b
fengxue <fengxue1876@gmail.com>
2019-11-25 16:50:09 +0900

    ルールに対する修正
    ルールに対する修正
    変数名
    書き方などの修正

--- Rhythm Fighters/Assets/Scripts/arrow1Controller.cs
+++ Rhythm Fighters/Assets/Scripts/arrow1Controller.cs
@@ -6,35 +6,30 @@ using UnityEngine.SceneManagement;
 
 public class arrow1Controller : MonoBehaviour
 {
-    public int charNum_1 = 1;
-    public int charNum_2 = 6;
-    public GameObject[] characters = new GameObject[6];
-    public GameObject arrow1;
-    public GameObject arrow2;
-    public bool player1PCharacter = false;
-    public bool player2PCharacter = false;
-    public Image player1_Icon;
-    public Image player2_Icon;
-    public Image player1_Comment;
-    public Image player2_Comment;
-    public Sprite[] player_Comments = new Sprite[6];
-    public Sprite[] player_Icons = new Sprite[6];
+    [SerializeField] private int charNum1 = 0,  charNum2 = 5, charNumMAX = 6;
 
-
+      [SerializeField] private int charNum_1 = 0, charNum_2 = 5;//いま選択しているキャラクター
+    [SerializeField] private GameObject[] characters = new GameObject[6];//キャラクター達
+    [SerializeField] private GameObject arrow1, arrow2, Background;//今だれか何か選択してる　赤い 青い
+    [SerializeField] private bool player1PCharacter = false, player2PCharacter = false;//選択確認
+    [SerializeField] private Image player1_Icon, player2_Icon;//選択したキャラクター
+    [SerializeField] private Sprite[] player_Icons = new Sprite[6], Backgrounds = new Sprite[3];//キャラクター画像
 
     //選択されたキャラのナンバー数を設定
 
     // Start is called before the first frame update
     void Start()
     {
+        charNum_1 = charNum1 % charNumMAX;
         arrow1 = GameObject.Find("arrow1");
-        arrow2 = GameObject.Find("arrow2");
         arrow1.transform.position = new Vector3(characters[charNum_1].transform.position.x, 320, 0);
-        arrow2.transform.position = new Vector3(characters[charNum_2].transform.position.x, 760, 0);
         player1_Icon.sprite = player_Icons[charNum_1];
-        player1_Comment.sprite = player_Comments[charNum_1];
+
+        charNum_2 = charNum2 % charNumMAX;
+        arrow2 = GameObject.Find("arrow2");
+        arrow2.transform.position = new Vector3(characters[charNum_2].transform.position.x, 760, 0);
         player2_Icon.sprite = player_Icons[charNum_2];
-        player2_Comment.sprite = player_Comments[charNum_2];
+
     }
 
     // Update is called once per frame
@@ -42,6 +37,7 @@ public class arrow1Controller : MonoBehaviour
     {
         Player2Move();
         Player1Move();
+
         if (player1PCharacter && player2PCharacter)
         {
             Debug.Log("1P" + characters[charNum_1] + "2P" + characters[charNum_2]);
@@ -54,120 +50,109 @@ public class arrow1Controller : MonoBehaviour
     void Player1Move()
     {
         //--------Player1 Y320
-        if (!player1PCharacter)
+
+        if (Input.GetKeyDown(KeyCode.A) && !player1PCharacter)
         {
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                //左へ移動
-                characters[charNum_1].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
-                charNum_1--;
-
-                if (charNum_1 < 0)
-                {
-                    charNum_1 = 5;
-                }
-                if (charNum_2 == charNum_1)
-                {
-                    charNum_1--;
-                    if (charNum_1 < 0)
-                    {
-                        charNum_1 = 5;
-                    }
-                }
-
-
+            characters[charNum_1].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
+            //左へ移動
+            charNum1--;
+         
+            charNum_1 = charNum1 % charNumMAX;
+            if (charNum1 % charNumMAX <= 0) {
+                charNum1 = charNumMAX;
             }
-            else if (Input.GetKeyDown(KeyCode.D))
+            if (charNum1 % charNumMAX == charNum2 % charNumMAX)
             {
-                characters[charNum_1].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
-                charNum_1++;
-
-                if (charNum_1 > 5)
-                {
-                    charNum_1 = 0;
-                }
-                if (charNum_1 == charNum_2)
-                {
-                    charNum_1++;
-                    if (charNum_1 > 5)
-                    {
-                        charNum_1 = 0;
-                    }
-                }
-
-
-            }
-            arrow1.transform.position = new Vector3(characters[charNum_1].transform.position.x, 320, 0);
-            player1_Icon.sprite = player_Icons[charNum_1];
-            player1_Comment.sprite = player_Comments[charNum_1];
-
-            if (Input.GetKeyDown(KeyCode.E) && !player1PCharacter)
-            {
-                player1PCharacter = true;
-
-            }
-            if (player1PCharacter) { characters[charNum_1].GetComponent<Image>().color = new Color(255.0f / 255.0f, 8 / 255.0f, 0 / 255.0f, 255 / 255.0f); }
-            else
-            {
-                characters[charNum_1].GetComponent<Image>().color = new Color(255.0f / 255.0f, 8 / 255.0f, 0 / 255.0f, 130 / 255.0f);
+                charNum1--;
             }
         }
+        else if (Input.GetKeyDown(KeyCode.D) && !player1PCharacter)
+        {
+            characters[charNum_1].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
+            charNum1++;
+           
+            if (charNum1 % charNumMAX == charNum2 % charNumMAX)
+            {
+                charNum1++;
+            }
+
+        }
+        charNum_1 = charNum1 % charNumMAX;
+        //
+        player1_Icon.sprite = player_Icons[charNum_1];
+        arrow1.transform.position = new Vector3(characters[charNum_1].transform.position.x, 320, 0);
+       
+
+
+       
+        if (Input.GetKeyDown(KeyCode.E) && !player1PCharacter)
+        {
+            player1PCharacter = true;
+
+        } //」選択するまだ選択直し処理
+        else if (Input.GetKeyDown(KeyCode.E) && player1PCharacter)
+        {
+            player1PCharacter = false;
+        }
+       
+        if (player1PCharacter)
+        {
+            characters[charNum_1].GetComponent<Image>().color = new Color(255.0f / 255.0f, 8 / 255.0f, 0 / 255.0f, 255 / 255.0f);
+        } //選択した表示
+        else
+        {
+            characters[charNum_1].GetComponent<Image>().color = new Color(255.0f / 255.0f, 8 / 255.0f, 0 / 255.0f, 130 / 255.0f);
+        }
+
     }
     void Player2Move()
     {
-        if (!player2PCharacter)
+
+        //-----------------Player2Y760
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && !player2PCharacter)
         {
-            //-----------------Player2Y760
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            characters[charNum_2].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
+            //左へ移動
+            charNum2--;
+
+         
+            if (charNum2 % charNumMAX <= 0)
             {
-                characters[charNum_2].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
-                charNum_2--;
-
-                if (charNum_2 < 0)
-                {
-                    charNum_2 = 5;
-                }
-                if (charNum_2 == charNum_1)
-                {
-                    charNum_2--;
-                    if (charNum_2 < 0)
-                    {
-                        charNum_2 = 5;
-                    }
-                }
+                charNum2 = charNumMAX;
             }
-            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            if (charNum2 % charNumMAX == charNum1 % charNumMAX)
             {
-                characters[charNum_2].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
-                //左へ移動
-                charNum_2++;
-                if (charNum_2 > 5)
-                {
-                    charNum_2 = 0;
-                }
-                if (charNum_2 == charNum_1)
-                {
-                    charNum_2++;
-                    if (charNum_2 > 5)
-                    {
-                        charNum_2 = 0;
-                    }
-                }
-
-
+                charNum2--;
             }
-
-            arrow2.transform.position = new Vector3(characters[charNum_2].transform.position.x, 760, 0);
-            player2_Icon.sprite = player_Icons[charNum_2];
-            player2_Comment.sprite = player_Comments[charNum_2];
-
-            if (Input.GetKeyDown(KeyCode.Space) && !player2PCharacter)
-            {
-                player2PCharacter = true;
-
-            }
-            if (player2PCharacter) { characters[charNum_2].GetComponent<Image>().color = new Color(0 / 255.0f, 62 / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f); }
-            else { characters[charNum_2].GetComponent<Image>().color = new Color(0 / 255.0f, 62 / 255.0f, 255.0f / 255.0f, 130.0f / 255.0f); }
         }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && !player2PCharacter)
+        {
+            characters[charNum_2].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
+            charNum2++;
+
+            if (charNum1 % charNumMAX == charNum2 % charNumMAX)
+            {
+                charNum2++;
+            }
+
+        }
+        charNum_2 = charNum2 % charNumMAX;
+        arrow2.transform.position = new Vector3(characters[charNum_2].transform.position.x, 760, 0);
+        player2_Icon.sprite = player_Icons[charNum_2];
+
+        if (Input.GetKeyDown(KeyCode.Space) && !player2PCharacter)
+        {
+            player2PCharacter = true;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && player2PCharacter)
+        {
+            player2PCharacter = false;
+        }
+        if (player2PCharacter) { characters[charNum_2].GetComponent<Image>().color = new Color(0 / 255.0f, 62 / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f); }
+        else { characters[charNum_2].GetComponent<Image>().color = new Color(0 / 255.0f, 62 / 255.0f, 255.0f / 255.0f, 130.0f / 255.0f); }
+
     }
+
+
 }
