ad410c4ec943cfdbf3f1604561c2c3031d78ee2e
yata <yuuki0523yata@gmail.com>
2019-12-06 18:48:23 +0900

    打消しモードの分岐用switchまで作成

---
+++ Rhythm Fighters/Assets/Member/Iwasaki/Scripts/NegationMode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NegationMode : MonoBehaviour
+{
+    //どの攻撃判定なったかをint値で管理
+    private int negationModeNum;
+    //打消しモードに入ったときにbool値をもらう
+    public static bool check = false;
+
+    private void Update()
+    {
+        if(check == true)
+        {
+            NegationModeStart();
+        }
+    }
+
+    private void NegationModeStart()
+    {
+        switch (negationModeNum)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+    }
+}
