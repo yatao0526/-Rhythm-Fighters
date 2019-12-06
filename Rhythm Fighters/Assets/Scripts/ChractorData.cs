b954b2431a5ed3be0e95d143cece2ff909fdb237
yata <yuuki0523yata@gmail.com>
2019-12-06 17:14:58 +0900

    キャラクターののけぞり管理するscriptable作成

---
+++ Rhythm Fighters/Assets/Scripts/ChractorData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName ="CreateCharactorData")]
+public class ChractorData : ScriptableObject
+{
+    
+    public enum Warping
+    {
+        Small,
+        Middle,
+        Big
+    }
+    [SerializeField]
+    private Warping warping = default(Warping);
+    public Warping _war { get => warping; }
+}
