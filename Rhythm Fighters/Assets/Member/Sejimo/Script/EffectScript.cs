7e311b8c7f40625f7e1a28f497ceafb703208820
sejimomizuki <sejimo.615@gmail.com>
2019-12-15 02:04:05 +0900

    いつでもエフェクトのアニメーションを入れ込められるようにした

---
+++ Rhythm Fighters/Assets/Member/Sejimo/Script/EffectScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectScript : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MissFX()
+    {
+
+    }
+
+    public void HpFx()
+    {
+
+    }
+
+    public void S2Fx()
+    {
+
+    }
+
+    public void PoseFx()
+    {
+
+    }
+}
