ba218ab81c2982be908a21dd402104ab7d90db9d
Tomioka <17c1020019tt@edu.tech.ac.jp>
2020-01-18 22:00:06 +0900

    プレイヤーのおかしくなってるところ修正

--- Rhythm Fighters/Assets/Member/Tomioka/Scripts/Player2AttackManager.cs
+++ Rhythm Fighters/Assets/Member/Tomioka/Scripts/Player2AttackManager.cs
@@ -10,7 +10,7 @@ public class Player2AttackManager : MonoBehaviour
     {
         animator = GetComponent<Animator>();
     }
-    
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log(col.name);
@@ -19,17 +19,17 @@ public class Player2AttackManager : MonoBehaviour
             case "LightPunch":
                 Debug.Log("1Pの弱攻撃が当たった");
                 animator.SetTrigger("Trigger_knock1");
-                //HPManager.player2HP -= 100;
+                HPManager.player2HP -= 100;
                 break;
             case "HeavyPunch":
                 Debug.Log("1Pの強攻撃が当たった");
                 this.animator.SetTrigger("Trigger_knock2");
-                //HPManager.player2HP -= 100;
+                HPManager.player2HP -= 100;
                 break;
             case "Skill2":
                 Debug.Log("1Pのスキル2が当たった");
                 this.animator.SetTrigger("Trigger_knock3");
-                //HPManager.player2HP -= 100;
+                HPManager.player2HP -= 100;
                 break;
         }
     }
