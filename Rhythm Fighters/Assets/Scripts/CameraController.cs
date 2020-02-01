f13f0106a90052524734c4c347b3010f038880f6
TAN <@tech.c>
2020-02-01 12:02:52 +0900

    移動方法変更につれ、カメラ動き方変更（途中）

--- Rhythm Fighters/Assets/Scripts/CameraController.cs
+++ Rhythm Fighters/Assets/Scripts/CameraController.cs
@@ -9,10 +9,11 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private GameObject PlayerTwo;
 
+    private float AttenRate = 0.3f; // 減衰比率
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -27,11 +28,21 @@ public class CameraController : MonoBehaviour
         }
         else if (Mathf.Abs(PlayerOne.transform.position.x) >= Mathf.Abs(PlayerTwo.transform.position.x))
         {
-            Camera.main.fieldOfView = 10 + ((Mathf.Abs(PlayerOne.transform.position.x) - 6) / 6 * 8);
+            // Lerp減衰
+            //Time.deltaTime * AttenRate ====>> to nowPosition.x between 6 and 12 as  10 and 18
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, ((Mathf.Abs(PlayerOne.transform.position.x) - 6) / 6 * 8), Time.deltaTime * AttenRate);
+            //Camera.main.fieldOfView = 10 + ((Mathf.Abs(PlayerOne.transform.position.x) - 6) / 6 * 8);
         }
         else
         {
-            Camera.main.fieldOfView = 10 + ((Mathf.Abs(PlayerTwo.transform.position.x) - 6) / 6 * 8);
+            // Lerp減衰
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, ((Mathf.Abs(PlayerTwo.transform.position.x) - 6) / 6 * 8), Time.deltaTime * AttenRate);
+            //Camera.main.fieldOfView = 10 + ((Mathf.Abs(PlayerTwo.transform.position.x) - 6) / 6 * 8);
         }
+
+        // Lerp減衰
+        //Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, ((Mathf.Abs(PlayerTwo.transform.position.x) - 6) / 6 * 8), Time.deltaTime * AttenRate);
+
+
     }
 }
