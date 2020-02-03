8d599c24a16031a214e0f9269ad235a4e5c65221
TAN <@tech.c>
2020-02-03 11:46:52 +0900

    カメラ移動方法修正（完了）

--- Rhythm Fighters/Assets/Scripts/CameraController.cs
+++ Rhythm Fighters/Assets/Scripts/CameraController.cs
@@ -9,7 +9,7 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private GameObject PlayerTwo;
 
-    private float AttenRate = 0.3f; // 減衰比率
+    private float AttenRate = 3f; // 減衰比率
 
     // Start is called before the first frame update
     void Start()
@@ -19,30 +19,19 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Abs(PlayerOne.transform.position.x) <= 6 && Mathf.Abs(PlayerTwo.transform.position.x) <= 6)
+        if (Mathf.Abs(PlayerOne.transform.position.x) < 6 && Mathf.Abs(PlayerTwo.transform.position.x) < 6)
         {
             Camera.main.fieldOfView = 10;
-        }else if(Mathf.Abs(PlayerOne.transform.position.x)>=12|| Mathf.Abs(PlayerTwo.transform.position.x) >= 12)
-        {
-            Camera.main.fieldOfView = 18;
         }
         else if (Mathf.Abs(PlayerOne.transform.position.x) >= Mathf.Abs(PlayerTwo.transform.position.x))
         {
             // Lerp減衰
-            //Time.deltaTime * AttenRate ====>> to nowPosition.x between 6 and 12 as  10 and 18
-            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, ((Mathf.Abs(PlayerOne.transform.position.x) - 6) / 6 * 8), Time.deltaTime * AttenRate);
-            //Camera.main.fieldOfView = 10 + ((Mathf.Abs(PlayerOne.transform.position.x) - 6) / 6 * 8);
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, ((Mathf.Abs(PlayerOne.transform.position.x) - 6) / 6 * 8) + 10, Time.deltaTime * AttenRate);
         }
         else
         {
             // Lerp減衰
-            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, ((Mathf.Abs(PlayerTwo.transform.position.x) - 6) / 6 * 8), Time.deltaTime * AttenRate);
-            //Camera.main.fieldOfView = 10 + ((Mathf.Abs(PlayerTwo.transform.position.x) - 6) / 6 * 8);
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, ((Mathf.Abs(PlayerOne.transform.position.x) - 6) / 6 * 8) + 10, Time.deltaTime * AttenRate);
         }
-
-        // Lerp減衰
-        //Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, ((Mathf.Abs(PlayerTwo.transform.position.x) - 6) / 6 * 8), Time.deltaTime * AttenRate);
-
-
     }
 }
